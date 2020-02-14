297e8bbdb5032a8c40c2fc193a9632d9ef3dd3a7
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-02-14 15:37:29 +0100

    add character view starter content

--- src/routes/Characters.js
+++ src/routes/Characters.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Navbar from '../Components/Navbar';
+import s1 from '../Assets/images/danny.png'
 
 export class Characters extends Component {
     render() {
         return (
+            <>
            <Navbar />
+                <section className="characters">
+                    <figure className="item-1">
+                        <img src={s1} alt="Season one"/>
+                        <figcaption>Season 1</figcaption>
+                    </figure>
+                </section>
+            </>
         )
     }
 }
