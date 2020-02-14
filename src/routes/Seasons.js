a043d678dd65849745521f806fa89e08397f7c82
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-02-14 16:28:14 +0100

    add images for different seasons

--- src/routes/Seasons.js
+++ src/routes/Seasons.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import Navbar from '../Components/Navbar';
 import s1 from '../Assets/images/danny.png';
+import s2 from '../Assets/images/arya.png';
+import s3 from '../Assets/images/jon.png';
+import s4 from '../Assets/images/jamie.png';
+import s5 from '../Assets/images/sansa.png';
 import './Seasons.css';
 
 export class Seasons extends Component {
@@ -11,33 +15,33 @@ export class Seasons extends Component {
                 <Navbar />
                 <section className="Seasons">
                     <figure className="item-1">
-                        <NavLink to="/season-1">
+                        <NavLink to="/season1">
                             <img src={s1} alt="Season one" />
                             <figcaption>Season 1</figcaption>
                         </NavLink>
                     </figure>
                     <figure className="item-1">
-                        <NavLink to="/season-1">
-                            <img src={s1} alt="Season one" />
-                            <figcaption>Season 1</figcaption>
+                        <NavLink to="/season2">
+                            <img src={s2} alt="Season one" />
+                            <figcaption>Season 2</figcaption>
                         </NavLink>
                     </figure>
                     <figure className="item-1">
-                        <NavLink to="/season-1">
-                            <img src={s1} alt="Season one" />
-                            <figcaption>Season 1</figcaption>
+                        <NavLink to="/season3">
+                            <img src={s3} alt="Season one" />
+                            <figcaption>Season 3</figcaption>
                         </NavLink>
                     </figure>
                     <figure className="item-1">
-                        <NavLink to="/season-1">
-                            <img src={s1} alt="Season one" />
-                            <figcaption>Season 1</figcaption>
+                        <NavLink to="/season4">
+                            <img src={s4} alt="Season one" />
+                            <figcaption>Season 4</figcaption>
                         </NavLink>
                     </figure>
                     <figure className="item-1">
-                        <NavLink to="/season-1">
-                            <img src={s1} alt="Season one" />
-                            <figcaption>Season 1</figcaption>
+                        <NavLink to="/season5">
+                            <img src={s5} alt="Season one" />
+                            <figcaption>Season 5</figcaption>
                         </NavLink>
                     </figure>
                 </section>
