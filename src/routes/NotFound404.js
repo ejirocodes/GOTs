50b324cfb460af5fc71e7debc2b5cf7c618d0db0
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-02-14 10:41:22 +0100

    create 404 page

---
+++ src/routes/NotFound404.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './NotFound404.css'
+
+class NotFound404 extends Component {
+    render() {
+        return (
+            <section className="Not-Found">
+                
+            </section>
+        )
+    }
+}
+
+export default NotFound404;
