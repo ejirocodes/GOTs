4b93d4f990b94262e20ed635cda365368d9bc228
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-02-17 10:28:48 +0100

    charcters route starter files

---
+++ src/routes/Characters.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Characters extends Component {
+    render() {
+        return (
+            <>
+              <h1>All your favourite Characters</h1>  
+            </>
+        )
+    }
+}
+
+export default Characters
