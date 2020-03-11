8cd0eea069a6b06e22d5a47934ff1e799696e914
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 16:00:47 +0100

    update Houses component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Seasons from './routes/Seasons'
-import Gallery from './routes/Gallery'
-import Homepage from './Views/Homepage'
+import Seasons from './routes/Seasons';
+import Gallery from './routes/Gallery';
+import Homepage from './Views/Homepage';
 import Houses from './routes/Houses';
 import Characters from './routes/Characters';
 import Lore from './routes/Lore';
@@ -14,12 +14,12 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/" exact component={Homepage} />
-        <Route  path="/seasons" exact component={Seasons} />
-        <Route  path="/characters" exact component={Characters} />
-        <Route  path="/houses" exact component={Houses} />
-        <Route  path="/lore" exact component={Lore} />
-        <Route  path="/gallery" exact component={Gallery} />
-        <Route  path="*" exact component={NotFound404} />
+        <Route path="/seasons" exact component={Seasons} />
+        <Route path="/characters" exact component={Characters} />
+        <Route path="/houses" exact component={Houses} />
+        <Route path="/lore" exact component={Lore} />
+        <Route path="/gallery" exact component={Gallery} />
+        <Route path="*" exact component={NotFound404} />
       </Switch>
     </div>
   );
--- src/routes/Houses.js
+++ src/routes/Houses.js
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Houses.css';
 import card1 from '../Assets/images/Targaryan_Post.png';
 import card2 from '../Assets/images/Starrks.png';
 import card3 from '../Assets/images/boratheons.png';
 import card4 from '../Assets/images/lannasters.png';
+import backIcon from '../Assets/elements/back_icon.png';
 export class Houses extends Component {
   render() {
     return (
       <>
-        {/* <Navbar /> */}
         <section className="Houses">
-          <div className="Houses--bg-overlay">
-            <div className="Houses__card-container">
-              <img src={card1} alt="Targaryan Post" />
-              <img src={card2} alt="Starrks" />
-              <img src={card4} alt="Lannasters" />
-              <img src={card3} alt="Boratheons" />
-            </div>
+          <div className="back-btn">
+            <NavLink to="">
+              <p>BACK</p> <img src={backIcon} alt="" />{' '}
+            </NavLink>
+          </div>
+          <div className="Houses__card-container">
+            <NavLink to="/targaryan">
+              <figure>
+                <img src={card1} alt="Targaryan Post" />
+              </figure>
+            </NavLink>
+            <NavLink to="/starrks">
+              <figure>
+                <img src={card2} alt="Starrks" />
+              </figure>
+            </NavLink>
+            <NavLink to="/lannasters">
+              <figure>
+                <img src={card4} alt="Lannasters" />
+              </figure>
+            </NavLink>
+            <NavLink to="/boratheons">
+              <figure>
+                <img src={card3} alt="Boratheons" />
+              </figure>
+            </NavLink>
           </div>
         </section>
       </>
