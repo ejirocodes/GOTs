408995823ec22d32cf95d630fbe67d29ae275693
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 15:09:37 +0100

    position card images

--- src/routes/Houses.js
+++ src/routes/Houses.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Navbar from '../Components/Navbar';
 import './Houses.css';
 import card1 from '../Assets/images/Targaryan_Post.png';
 import card2 from '../Assets/images/Starrks.png';
@@ -7,16 +6,17 @@ import card3 from '../Assets/images/boratheons.png';
 import card4 from '../Assets/images/lannasters.png';
 export class Houses extends Component {
   render() {
-    const imgContainer = {};
     return (
       <>
-        <Navbar />
+        {/* <Navbar /> */}
         <section className="Houses">
-          <div className="Houses__card-container ">
-            <img src={card1} alt="Targaryan Post" />
-            <img src={card2} alt="Starrks" />
-            <img src={card3} alt="Boratheons" />
-            <img src={card4} alt="Lannasters" />
+          <div className="Houses--bg-overlay">
+            <div className="Houses__card-container">
+              <img src={card1} alt="Targaryan Post" />
+              <img src={card2} alt="Starrks" />
+              <img src={card4} alt="Lannasters" />
+              <img src={card3} alt="Boratheons" />
+            </div>
           </div>
         </section>
       </>
