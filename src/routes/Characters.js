23020c0511f3c732ab5c4fc5c930cd62f15f7b5d
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 17:26:46 +0100

    update characters component

--- src/routes/Characters.js
+++ src/routes/Characters.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import backIcon from '../Assets/elements/back_icon.png';
-import './Characters.css'
+import './Characters.css';
 
 class Characters extends Component {
   render() {
     return (
       <>
         <section className="Characters">
+          <div className="Characters__bg--left"></div>
           <div className="back-btn">
             <NavLink to="/">
               <p>BACK</p> <img src={backIcon} alt="" />
