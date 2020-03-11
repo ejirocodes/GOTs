0ad1600b97cc5b3d7198396c6118ace36f0c865d
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 14:00:35 +0100

    initial commit for Houses component

--- src/routes/Houses.js
+++ src/routes/Houses.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from '../Components/Navbar';
+import './Houses.css';
 
 export class Houses extends Component {
     render() {
         return (
-           <Navbar />
+            <>
+                <Navbar />
+            </>
         )
     }
 }
