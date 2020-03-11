1d937749e9e4d57c4daeedb4bc368bec3239ba97
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 16:54:59 +0100

    fix LearnMore component overflow

--- src/Components/LearnMore.js
+++ src/Components/LearnMore.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import LearnMoreImg from '../Assets/images/night_king_image.png'
-import './LearnMore.css'
-
-
+import LearnMoreImg from '../Assets/images/night_king_image.png';
+import './LearnMore.css';
 
 const LearnMore = () => {
-    return (
-        <section className="LearnMore-container">
-            <div className="learn-more-img">
-                <img src={LearnMoreImg} alt=""/>
-            </div>
-            <div className="learn-more-text">
-                <h2>Learn <br />
-                    More <br />
-                    About <br />
-                    The Show
-                </h2>
-                <a className="learn-more-btn"  href="https://www.hbo.com/game-of-thrones"  rel="noopener noreferrer" target="_blank">Learn More</a>
-            </div>
-        </section>
-   ) 
-}
+  return (
+    <section className="LearnMore-container">
+      <div className="learn-more-img">
+        <img src={LearnMoreImg} alt="" style={{ width: '100%' }} />
+      </div>
+      <div className="learn-more-text">
+        <h2>
+          Learn <br />
+          More <br />
+          About <br />
+          The Show
+        </h2>
+        <a
+          className="learn-more-btn"
+          href="https://www.hbo.com/game-of-thrones"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Learn More
+        </a>
+      </div>
+    </section>
+  );
+};
 
 export default LearnMore;
