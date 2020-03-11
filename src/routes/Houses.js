b33c94d9ad322d836ec9f9828f78df8fea957780
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 14:39:46 +0100

    configure eslint and prettier

--- src/routes/Houses.js
+++ src/routes/Houses.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
 import Navbar from '../Components/Navbar';
 import './Houses.css';
-import card1 from '../Assets/images/Targaryan_Post.png'
-import card2 from '../Assets/images/Starrks.png'
-import card3 from '../Assets/images/boratheons.png'
-import card4 from '../Assets/images/lannasters.png'
+import card1 from '../Assets/images/Targaryan_Post.png';
+import card2 from '../Assets/images/Starrks.png';
+import card3 from '../Assets/images/boratheons.png';
+import card4 from '../Assets/images/lannasters.png';
 export class Houses extends Component {
-    render() {
-        return (
-            <>
-                <Navbar />
-                <section className="Houses">
-                    <div className="Houses__card-container">
-                        <div className="House__card-img">
-                            <img src={card1} alt="Targaryan Post"/>
-                            <img src={card2} alt="Starrks"/>
-                            <img src={card3} alt="Boratheons"/>
-                            <img src={card4} alt="Lannasters"/>
-                        </div>
-                    </div>
-                </section>
-            </>
-        )
-    }
+  render() {
+    const imgContainer = {};
+    return (
+      <>
+        <Navbar />
+        <section className="Houses">
+          <div className="Houses__card-container ">
+            <img src={card1} alt="Targaryan Post" />
+            <img src={card2} alt="Starrks" />
+            <img src={card3} alt="Boratheons" />
+            <img src={card4} alt="Lannasters" />
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
-export default Houses
+export default Houses;
