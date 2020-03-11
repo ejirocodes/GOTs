e5ec9b6efccccaf471d47e670f7687ee2246c149
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-11 17:19:05 +0100

    character component css starter code

--- src/routes/Characters.js
+++ src/routes/Characters.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import backIcon from '../Assets/elements/back_icon.png';
+import './Characters.css'
 
 class Characters extends Component {
   render() {
     return (
       <>
-        <section className="Houses">
+        <section className="Characters Houses">
           <div className="back-btn">
             <NavLink to="/">
               <p>BACK</p> <img src={backIcon} alt="" />
