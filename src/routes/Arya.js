0fd70bcccfc7b19ef8410d0c7f4993030563262a
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-12 17:03:15 +0100

    update favicon

--- src/routes/Arya.js
+++ src/routes/Arya.js
@@ -1,15 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Arya.css';
+import backIcon from '../Assets/elements/back_icon.png';
 
 export class Arya extends Component {
-    render() {
-        return (
-            <section>
-                This
-                 <div>is</div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section className="Arya">
+        <div className="back-btn">
+          <NavLink to="/">
+            <p>BACK</p> <img src={backIcon} alt="" />
+          </NavLink>
+        </div>
+        <div className="Arya-right">
+          <h2>" STICK'M WITH <br/> THE POITY END "</h2>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default Arya
+export default Arya;
