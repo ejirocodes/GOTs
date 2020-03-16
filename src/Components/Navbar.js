1415d5cba613f13d050da9996a7931b91adc4180
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-16 09:54:25 +0100

    responsive navbar, header && LearnMore component

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,37 +1,47 @@
+import { NavLink } from 'react-router-dom';
 import React, { Component } from 'react';
-import {NavLink} from 'react-router-dom'
 import logo from '../Assets/elements/Logo_HBO@2x.png';
-import './Navbar.css'
+import './Navbar.css';
 
 export class Navbar extends Component {
-    render() {
-        return (
-            <>
-               <nav className="Nav-main"> 
-                    <NavLink className="Nav-logo" exact to="/">
-                        <img src={logo} alt="logo" title="HBO" />
-                    </NavLink>
-                    <ul className="Navlink-container">
-                        <li>
-                            <NavLink activeClassName="Nav-active" exact to='/seasons'>Seasons</NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeClassName="Nav-active" exact to='/characters'>Characters</NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeClassName="Nav-active" exact to='/houses'>Houses</NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeClassName="Nav-active" exact to='/lore'>Lore</NavLink>
-                        </li>
-                        <li>
-                            <NavLink activeClassName="Nav-active" exact to='/gallery'>Gallery</NavLink>
-                        </li>
-                    </ul>
-                </nav> 
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <nav className="Nav-main">
+          <NavLink className="Nav-logo" exact to="/">
+            <img src={logo} alt="logo" title="HBO" />
+          </NavLink>
+          <ul className="Navlink-container">
+            <li>
+              <NavLink activeClassName="Nav-active" exact to="/seasons">
+                Seasons
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="Nav-active" exact to="/characters">
+                Characters
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="Nav-active" exact to="/houses">
+                Houses
+              </NavLink>
+            </li>
+            <li>
+              <NavLink activeClassName="Nav-active" exact to="/lore">
+                Lore
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="last-nav-item" activeClassName="Nav-active" exact to="/gallery">
+                Gallery
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
