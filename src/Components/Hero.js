9b8d9b64bc692169eccb3c74ee16f0aa2852edc2
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-19 15:22:27 +0100

    render TrailerButton

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,45 +1,46 @@
 import React, { Component } from 'react';
 import './Hero.css';
-import PlayIcon from '../Assets/elements/play_icon.png'
-import FacebookIcon from '../Assets/elements/facebook@2x.png'
-import InstaIcon from '../Assets/elements/insta@2x.png'
-import NetflixIcon from '../Assets/elements/netflix@2x.png'
-import YoutubeIcon from '../Assets/elements/Youtube@2x.png'
+import FacebookIcon from '../Assets/elements/facebook@2x.png';
+import InstaIcon from '../Assets/elements/insta@2x.png';
+import NetflixIcon from '../Assets/elements/netflix@2x.png';
+import YoutubeIcon from '../Assets/elements/Youtube@2x.png';
+import TrailerButton from './TrailerButton';
 
 export class Hero extends Component {
-    render() {
-        return (
-            <div className="Hero-main">
-                <div className="Hero-left">
-                    <h1>Game of <br /> Thrones</h1>
-                    <h2>
-                        "There is only one war that matters. <br />
-                        The Great War. And it is here." ~ Jon Snow
-                    </h2>
-                    <a href="https://www.youtube.com/watch?v=rlR4PJn8b8I" target="_blank" rel="noopener noreferrer" className="cta-btn">
-                        <span className="play-icon" role="img">
-                            <img src={PlayIcon} alt="play icon" /> 
-                        </span> <p>Watch Trailer</p>
-                    </a>
-                </div>
-                <div className="Hero-right">
-                    <a href="http://" title="Game of Thrones on Facebook">
-                        <img src={FacebookIcon} alt="Facebook page" />
-                    </a>
-                     <a href="http://" title="Game of Thrones on Instagram">
-                        <img src={InstaIcon} alt="Instagram page" />
-                    </a>
-                     <a href="http://" title="Game of Thrones on Youtube">
-                        <img src={YoutubeIcon} alt="Youtube channel" />
-                    </a>
-                     <a href="http://" title="Game of Thrones on Netflix">
-                        <img className="netflix-img" src={NetflixIcon} alt="Stream on netflix" />
-                    </a>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="Hero-main">
+        <div className="Hero-left">
+          <h1>
+            Game of <br /> Thrones
+          </h1>
+          <h2>
+            "There is only one war that matters. <br />
+            The Great War. And it is here." ~ Jon Snow
+          </h2>
+          <TrailerButton />
+        </div>
+        <div className="Hero-right">
+          <a href="http://" title="Game of Thrones on Facebook">
+            <img src={FacebookIcon} alt="Facebook page" />
+          </a>
+          <a href="http://" title="Game of Thrones on Instagram">
+            <img src={InstaIcon} alt="Instagram page" />
+          </a>
+          <a href="http://" title="Game of Thrones on Youtube">
+            <img src={YoutubeIcon} alt="Youtube channel" />
+          </a>
+          <a href="http://" title="Game of Thrones on Netflix">
+            <img
+              className="netflix-img"
+              src={NetflixIcon}
+              alt="Stream on netflix"
+            />
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Hero
-
+export default Hero;
--- src/Components/TrailerButton.js
+++ src/Components/TrailerButton.js
@@ -1,5 +1,25 @@
- <a href="https://www.youtube.com/watch?v=rlR4PJn8b8I" target="_blank" rel="noopener noreferrer" className="cta-btn">
-                        <span className="play-icon" role="img">
-                            <img src={PlayIcon} alt="play icon" /> 
-                        </span> <p>Watch Trailer</p>
-</a>
+import React, { Component } from 'react';
+import PlayIcon from '../Assets/elements/play_icon.png';
+
+export class TrailerButton extends Component {
+    render() {
+        return (
+          <React.Fragment>
+            <a
+              href="https://www.youtube.com/watch?v=rlR4PJn8b8I"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="cta-btn"
+            >
+              <span className="play-icon" role="img">
+                <img src={PlayIcon} alt="play icon" />
+              </span>{' '}
+              <p>Watch Trailer</p>
+            </a>
+          </React.Fragment>
+        );
+    }
+}
+
+export default TrailerButton
+
