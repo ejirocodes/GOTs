e304c7ddf44c18fb353f5377780920690fea6aea
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-19 15:28:46 +0100

    Add video modal

--- src/Components/TrailerButton.js
+++ src/Components/TrailerButton.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
+import ModalVideo from 'react-modal-video';
 import PlayIcon from '../Assets/elements/play_icon.png';
 
 export class TrailerButton extends Component {
-    render() {
-        return (
-          <React.Fragment>
-            <a
-              href="https://www.youtube.com/watch?v=rlR4PJn8b8I"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn"
-            >
-              <span className="play-icon" role="img">
-                <img src={PlayIcon} alt="play icon" />
-              </span>{' '}
-              <p>Watch Trailer</p>
-            </a>
-          </React.Fragment>
-        );
-    }
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    };
+    this.openModal = this.openModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({ isOpen: true });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <ModalVideo
+          channel="youtube"
+          isOpen={this.state.isOpen}
+          videoId="rlR4PJn8b8I"
+          onClose={() => this.setState({ isOpen: false })}
+        />
+        <a
+          href="javascript:void(0)"
+          //   target="_blank"
+          //   rel="noopener noreferrer"
+          className="cta-btn"
+          onClick={this.openModal}
+        >
+          <span className="play-icon" role="img">
+            <img src={PlayIcon} alt="play icon" />
+          </span>{' '}
+          <p>Watch Trailer</p>
+        </a>
+      </React.Fragment>
+    );
+  }
 }
 
-export default TrailerButton
-
+export default TrailerButton;
