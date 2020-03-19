781828d02b056f12131354f020d424210079638a
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-19 15:03:23 +0100

    extract TrailerButton to a component, install react modal video

---
+++ src/Components/TrailerButton.js
@@ -0,0 +1,5 @@
+ <a href="https://www.youtube.com/watch?v=rlR4PJn8b8I" target="_blank" rel="noopener noreferrer" className="cta-btn">
+                        <span className="play-icon" role="img">
+                            <img src={PlayIcon} alt="play icon" /> 
+                        </span> <p>Watch Trailer</p>
+</a>
