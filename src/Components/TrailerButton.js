7fe0bb1ec055e58430532e85345f49c08cd6def7
E J I R O <Ejiroasiuwhu10@gmail.com>
2020-03-20 09:14:46 +0100

    install react bootstrap && add basic modal

--- src/Components/TrailerButton.js
+++ src/Components/TrailerButton.js
@@ -1,41 +1,32 @@
-import React, { Component } from 'react';
-import ModalVideo from 'react-modal-video';
+import React from 'react';
+import Modal from 'react-bootstrap/Modal';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import PlayIcon from '../Assets/elements/play_icon.png';
 
-export class TrailerButton extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isOpen: false
-    };
-    this.openModal = this.openModal.bind(this);
-  }
+const TrailerButton = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const showModal = () => {
+    setIsOpen(true);
+  };
+  const hideModal = () => {
+    setIsOpen(false);
+  };
+  return (
+    <React.Fragment>
+      <Modal show={isOpen} onHide={hideModal}>
+        <Modal.Header>Hi</Modal.Header>
+        <Modal.Body>asdfasdf</Modal.Body>
+        <Modal.Footer>This is the footer</Modal.Footer>
+      </Modal>
 
-  openModal() {
-    this.setState({ isOpen: true });
-  }
-  render() {
-    return (
-      <React.Fragment>
-        <ModalVideo
-          channel="youtube"
-          isOpen={this.state.isOpen}
-          videoId="rlR4PJn8b8I"
-          playlist="null"
-          maxheight="10px"
-          maxwidth="15px"
-          theme='light'
-          onClose={() => this.setState({ isOpen: false })}
-        />
-        <button className="cta-btn" onClick={this.openModal}>
-          <span className="play-icon" role="img">
-            <img src={PlayIcon} alt="play icon" />
-          </span>{' '}
-          <p>Watch Trailer</p>
-        </button>
-      </React.Fragment>
-    );
-  }
-}
+      <button onClick={showModal} className="cta-btn">
+        <span className="play-icon" role="img">
+          <img src={PlayIcon} alt="play icon" />
+        </span>
+        <p>Watch Trailer</p>
+      </button>
+    </React.Fragment>
+  );
+};
 
 export default TrailerButton;
